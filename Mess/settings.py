6b4541a52d80834bea36af7007291b2fa2fc2faf
Alexey Bojhev <9medved@mail.ru>
2018-04-23 17:12:01 +0300

    Woops
    Lets delete some SECRET_KEY

--- Mess/settings.py
+++ Mess/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#jj5%b60dqb2l2%t8&uw8z@$dkiwd23h-%ble*465eky6err1y'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
